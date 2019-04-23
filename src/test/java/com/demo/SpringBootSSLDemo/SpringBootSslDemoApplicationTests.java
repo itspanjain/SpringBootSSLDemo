f63742119652870b874818a7fbcf3eedb2302fb4
Pankaj <jainpankaj_15m@yahoo.com>
2019-04-24 00:41:50 +0530

    SSL Test cases added using mockmvc

--- src/test/java/com/demo/SpringBootSSLDemo/SpringBootSslDemoApplicationTests.java
+++ src/test/java/com/demo/SpringBootSSLDemo/SpringBootSslDemoApplicationTests.java
@@ -1,16 +1,56 @@
 package com.demo.SpringBootSSLDemo;
 
+import com.demo.SpringBootSSLDemo.Controllers.SSLController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class SpringBootSslDemoApplicationTests {
+
+	@Autowired
+	SSLController sslController;
+
+	@Autowired
+	MockMvc mockMvc;
+
+	/*static {
+		//for localhost testing only
+		javax.net.ssl.HttpsURLConnection.setDefaultHostnameVerifier(
+				new javax.net.ssl.HostnameVerifier(){
+					public boolean verify(String hostname, javax.net.ssl.SSLSession sslSession) {
+						if (hostname.equals("localhost")) {
+							return true;
+						}
+						return false;
+					}
+				});
+	}*/
 
 	@Test
 	public void contextLoads() {
+		assertThat(sslController).isNotNull();
+	}
+
+	@Test
+	public void sslGetMessage() throws Exception {
+		this.mockMvc.perform(get("/secured")).andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().contentType("text/plain;charset=UTF-8"))
+				.andExpect(content().string(containsString("Welcome User")));
 	}
 
 }
